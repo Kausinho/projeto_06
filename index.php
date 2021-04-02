0491c96526d1f257e69bcc4c43aa00616b799f54
Kausinho <kausinhobrunow@gmail.com>
2021-04-02 16:50:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
     <link href="css/font-awesome.min.css" rel="stylesheet" />
     <link href="css/style.css" rel="stylesheet" />
@@ -31,7 +31,146 @@
                 </nav>
                 <div class="clear"></div><!--clear-->
             </header>
+            <div class="conteudo-header">
+                    <h2>Bem-vindo(a) ao nosso <span>website!</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque congue ante ipsum, eu fermentum risus pretium in. Vestibulum gravida leo nec libero sagittis, molestie tincidunt nisi congue. Aliquam erat volutpat. Nam faucibus viverra ipsum, eget dignissim felis venenatis ut. Aenean placerat at eros id semper. Proin molestie tortor at magna commodo, in placerat quam bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque congue ante ipsum, eu fermentum risus pretium in. Vestibulum gravida leo nec libero sagittis, molestie tincidunt nisi congue. Aliquam erat volutpat. Nam faucibus viverra ipsum, eget dignissim felis venenatis ut. Aenean placerat at eros id semper. Proin molestie tortor at magna commodo, in placerat quam bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    <a href="">Saiba mais</a>
+                </div><!--conteudo-header-->
         </div><!--center-->
     </section><!--main-->
+
+    <section class="planos">
+        <div class="chamada-planos">
+            <div class="center">
+                <h2>Confira nossos planos</h2>
+            </div><!--center-->
+        </div><!--chamada-planos-->
+        <section class="box-planos">
+            <div class="center">
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                    <h2>Lorem Impsum</h2>
+                        <div class="pontos-plano">
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                        </div><!--pontos-planos-->
+                        <div class="preco">
+                            <h2>R$250,00</h2>
+                        </div><!--preco-->
+                    </div><!--box-planos-wraper-->
+                </div><!--box-planos-single-->
+
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                    <h2>Lorem Impsum</h2>
+                        <div class="pontos-plano">
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                        </div><!--pontos-planos-->
+                        <div class="preco">
+                            <h2>R$250,00</h2>
+                        </div><!--preco-->
+                    </div><!--box-planos-wraper-->
+                </div><!--box-planos-single-->
+
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                    <h2>Lorem Impsum</h2>
+                        <div class="pontos-plano">
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                            <p><i class="fa fa-check-square"></i>Lorem Ipsum silor domor.</p>
+                        </div><!--pontos-planos-->
+                        <div class="preco">
+                            <h2>R$250,00</h2>
+                        </div><!--preco-->
+                    </div><!--box-planos-wraper-->
+                </div><!--box-planos-single-->
+            </div><!--center-->
+        </section><!--box-planos-->
+    </section><!--planos-->
+
+    <section class="parallax">
+        <div class="overlay-parallax"></div><!--overlay-parallax-->
+        <div class="center">
+            <h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et nulla vitae dolor tristique rutrum et eu quam. Curabitur nec neque nec urna imperdiet tincidunt id vel tortor. Quisque vel mi id sapien semper porttitor. Vivamus elementum, massa nec varius efficitur, tortor velit efficitur diam, at facilisis libero enim in odio. Morbi a dolor sem. Donec gravida augue id nulla gravida luctus. Fusce vehicula purus non semper rhoncus. Sed elementum, elit quis vestibulum tempor, ligula mi dignissim dui, quis dignissim metus purus non sem. Nam non mauris ante.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et nulla vitae dolor tristique rutrum et eu quam. Curabitur nec neque nec urna imperdiet tincidunt id vel tortor. Quisque vel mi id sapien semper porttitor. Vivamus elementum, massa nec varius efficitur, tortor velit efficitur diam, at facilisis libero enim in odio. Morbi a dolor sem. Donec gravida augue id nulla gravida luctus. Fusce vehicula purus non semper rhoncus. Sed elementum, elit quis vestibulum tempor, ligula mi dignissim dui, quis dignissim metus purus non sem. Nam non mauris ante. Nam sit amet facilisis eros.</p>
+        </div><!--center-->
+    </section><!--parallax-->
+
+    <section class="final-section">
+        <div class="center">
+            <div class="w50 contato">
+                <h2><div class="borda-efeito"></div><!--border-efeito-->Entre em contato!<h2>
+                <form>
+                    <input type="text" placeholder="Insisra seu nome..." />
+                    <input type="email" placeholder="Insira seu email..." />
+                    <textarea placeholder="Sua mensagem..."></textarea>
+                    <input type="submit" value="Enviar!" />
+                </form>
+            </div><!--w50-->
+
+            <div class="w50 time">
+                <h2><div class="borda-efeito"></div><!--border-efeito-->Conheça nosso time!</h2>
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div><!--box-avatar--><div class="info-usuario">
+                            <p style="font-weight: bold;">Guilherme Grillo</p>
+                            <p style="font-weight: italic;">CEO / Instrutor</p>
+                        </div><!--info-usuario-->
+                    </div><!--topo-box-usuario-->
+                    <div class="descricao-usuario-box">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et nulla vitae dolor tristique rutrum et eu quam. Curabitur nec neque nec urna imperdiet tincidunt id vel tortor. Quisque vel mi id sapien semper porttitor.</p>
+                    </div><!--descricao-usuario-box-->
+                </div><!--w50-equipe-single-->
+
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div><!--box-avatar--><div class="info-usuario">
+                            <p style="font-weight: bold;">Guilherme Grillo</p>
+                            <p style="font-weight: italic;">CEO / Instrutor</p>
+                        </div><!--info-usuario-->
+                    </div><!--topo-box-usuario-->
+                    <div class="descricao-usuario-box">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et nulla vitae dolor tristique rutrum et eu quam. Curabitur nec neque nec urna imperdiet tincidunt id vel tortor. Quisque vel mi id sapien semper porttitor.</p>
+                    </div><!--descricao-usuario-box-->
+                </div><!--w50-equipe-single-->
+
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div><!--box-avatar--><div class="info-usuario">
+                            <p style="font-weight: bold;">Guilherme Grillo</p>
+                            <p style="font-weight: italic;">CEO / Instrutor</p>
+                        </div><!--info-usuario-->
+                    </div><!--topo-box-usuario-->
+                    <div class="descricao-usuario-box">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et nulla vitae dolor tristique rutrum et eu quam. Curabitur nec neque nec urna imperdiet tincidunt id vel tortor. Quisque vel mi id sapien semper porttitor.</p>
+                    </div><!--descricao-usuario-box-->
+                </div><!--w50-equipe-single-->
+
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div><!--box-avatar--><div class="info-usuario">
+                            <p style="font-weight: bold;">Guilherme Grillo</p>
+                            <p style="font-weight: italic;">CEO / Instrutor</p>
+                        </div><!--info-usuario-->
+                    </div><!--topo-box-usuario-->
+                    <div class="descricao-usuario-box">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et nulla vitae dolor tristique rutrum et eu quam. Curabitur nec neque nec urna imperdiet tincidunt id vel tortor. Quisque vel mi id sapien semper porttitor.</p>
+                    </div><!--descricao-usuario-box-->
+                </div><!--w50-equipe-single-->
+                <div class="clear"></div><!--clear-->
+            </div><!--w50-contato-->
+            <div class="clear"></div><!--clear-->
+        </div><!--center-->
+    </section><!--final-section-->
 </body>
 </html>
